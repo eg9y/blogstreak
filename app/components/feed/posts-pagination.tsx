caa12385a500b9ef9793a25920a67f03a6c7f2fa
Egan Bisma <egan@hey.com>
2024-03-29 11:17:31 +0700

    improve pagination logic

--- app/components/feed/posts-pagination.tsx
+++ app/components/feed/posts-pagination.tsx
@@ -1,61 +1,70 @@
-"use client";
-
+import { useMemo } from "react";
+import { useSearchParams, usePathname } from "next/navigation";
+import { Pagination, PaginationNext, PaginationPrevious } from "../pagination";
 import { getUser } from "@/utils/getUser";
 import { usePostsQuery } from "@/utils/hooks/query/use-posts-query";
-import { usePathname, useSearchParams } from "next/navigation";
-import { Pagination, PaginationNext, PaginationPrevious } from "../pagination";
 
 export function PostsPagination() {
   const { currentUser } = getUser();
   const searchParams = useSearchParams();
-  const username = usePathname().slice(1);
+  const pathName = usePathname();
+
+  const username = pathName.slice(1);
+  const itemsPerPage = 10;
 
   const currentPage = parseInt(searchParams.get("page") || "1", 10);
-  const itemsPerPage = 10; // Assuming this is defined and matches your pagination logic.
-
   const { data, isLoading } = usePostsQuery(
     currentUser,
     searchParams,
     username,
   );
 
-  // Calculate the starting and ending indices for the currently displayed items.
-  const startItemIndex = (currentPage - 1) * itemsPerPage + 1;
-  const endItemIndex = data
-    ? Math.min(startItemIndex + itemsPerPage - 1, data.count || 0)
-    : 0;
+  const {
+    startItemIndex,
+    endItemIndex,
+    totalPages,
+    previousPageHref,
+    nextPageHref,
+  } = useMemo(() => {
+    const start = (currentPage - 1) * itemsPerPage + 1;
+    const end = data ? Math.min(start + itemsPerPage - 1, data.count || 0) : 0;
+    const total = data ? Math.ceil((data?.count || 0) / itemsPerPage) : 0;
+
+    const getHref = (newPage: number) => {
+      const newSearchParams = new URLSearchParams(searchParams.toString());
+      newSearchParams.set("page", newPage.toString());
+      return `${pathName}?${newSearchParams}`;
+    };
+
+    const previousPageHref = currentPage > 1 ? getHref(currentPage - 1) : null;
+    const nextPageHref = currentPage < total ? getHref(currentPage + 1) : null;
+
+    return {
+      startItemIndex: start,
+      endItemIndex: end,
+      totalPages: total,
+      previousPageHref,
+      nextPageHref,
+    };
+  }, [currentPage, data, itemsPerPage, pathName, searchParams]);
 
   return (
     <Pagination className="flex items-baseline">
-      <PaginationPrevious
-        href={
-          data?.count && startItemIndex > 1
-            ? `?page=${currentPage - 1}`
-            : undefined
-        }
-      />
+      <PaginationPrevious href={previousPageHref} />
       {!isLoading && data ? (
         <div className="text-sm dark:text-slate-200">
-          {/* Check if data exists and has a count to handle initial null case */}
-          {data.count ? (
+          {data?.count ? (
             <p>
               Showing {startItemIndex}-{endItemIndex} of {data.count}
             </p>
           ) : (
-            <p>Loading...</p>
+            "No data found."
           )}
         </div>
       ) : (
-        ""
+        "Loading..."
       )}
-
-      <PaginationNext
-        href={
-          data?.count && endItemIndex < data.count
-            ? `?page=${currentPage + 1}`
-            : undefined
-        }
-      />
+      <PaginationNext href={nextPageHref} />
     </Pagination>
   );
 }
