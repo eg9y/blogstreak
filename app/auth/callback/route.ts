7c3cf964fd324241c6cc9deef82ad726c2b060cf
Egan Bisma <egan@hey.com>
2024-04-14 19:01:21 +0700

    fix

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,12 +2,21 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import { type CookieOptions, createServerClient } from "@supabase/ssr";
 
-export async function GET() {
-  const { searchParams, origin } = new URL(
+export async function GET(request: Request) {
+  // Extract the original search parameters and origin from the request
+  const requestUrl = new URL(request.url);
+  const searchParams = new URLSearchParams(requestUrl.search);
+
+  // Determine the base URL based on the environment
+  const baseUrl =
     process.env.NODE_ENV === "production"
       ? "https://blogstreak.com"
-      : "http://localhost:3000",
-  );
+      : "http://localhost:3000";
+
+  // Create a new URL with the base URL and retain the original search parameters
+  const url = new URL(baseUrl);
+  url.search = searchParams.toString();
+
   const code = searchParams.get("code");
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get("next") ?? "/";
@@ -33,10 +42,11 @@ export async function GET() {
     );
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      return NextResponse.redirect(`${origin}${next}`);
+      // Use the original origin and next parameters for redirection
+      return NextResponse.redirect(`${url.origin}${next}`);
     }
   }
 
   // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+  return NextResponse.redirect(`${url.origin}/auth/auth-code-error`);
 }
