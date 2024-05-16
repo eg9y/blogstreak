5b1aa510623112b4a664a036b13210cc3fb4e9cf
Egan Bisma <egan@hey.com>
2024-05-16 17:15:12 +0700

    test

--- app/auth/chatgpt-login/page.tsx
+++ app/auth/chatgpt-login/page.tsx
@@ -1,13 +1,45 @@
 "use client";
 
+import { redirect, useSearchParams } from "next/navigation";
+
 import { Button } from "@/app/components/button";
-import { signUpWithGoogleChatgpt } from "@/utils/signUpWithGoogleChatgpt";
+import { createClient } from "@/utils/supabase/client";
 
 export default function ChatgptLogin() {
+  const searchParams = useSearchParams();
+
+  const signUpWithGoogleChatgpt = async () => {
+    if (!searchParams) {
+      return redirect("/?message=Could not authenticate user via Google");
+    }
+
+    const supabase = createClient();
+
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: searchParams.get("redirect_uri") as string,
+        queryParams: {
+          access_type: "offline",
+          prompt: "consent",
+          state: searchParams.get("state") as string,
+        },
+      },
+    });
+
+    if (error) {
+      return redirect("/?message=Could not authenticate user via Google");
+    }
+
+    return redirect(data.url);
+  };
+
   return (
     <div>
       <p>Sign in to ChatGPT Blogstreak plugin!</p>
-      <Button onClick={signUpWithGoogleChatgpt}>Sign in</Button>
+      <Button onClick={signUpWithGoogleChatgpt} disabled={!searchParams}>
+        Sign in
+      </Button>
     </div>
   );
 }
